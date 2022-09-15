b070969e9e13ff7271335f6ba84d1fd984027b53
soomni95 <rlatnals0507@gmail.com>
2022-09-16 00:23:58 +0900

    clone-coding-till-7.2

---
+++ src/routers/rootRouter.js
@@ -0,0 +1,13 @@
+import express from "express";
+import { getJoin, login, postJoin } from "../controllers/userController";
+import { home, search } from "../controllers/videoController";
+
+const globalRouter = express.Router();
+
+globalRouter.get("/", home);
+globalRouter.route("/join").get(getJoin).post(postJoin);
+globalRouter.get("/login", login);
+globalRouter.get("/search", search);
+
+export default globalRouter;
+
