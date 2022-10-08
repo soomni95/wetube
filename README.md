87fc37fdbf13444afe592ee86f43c3b65577e23f
soomni95 <rlatnals0507@gmail.com>
2022-10-09 01:21:03 +0900

    message

---
+++ README.md
@@ -0,0 +1,16 @@
+#Wetube Uploaded
+
+/ -> Home
+/join -> Join
+/login -> Login
+/search -> Search
+
+/users/:id -> See User
+/users/logout -> Log Out
+/users/edit -> Edit MY Profile
+/users/delete -> Delete MY User
+
+/videos/:id -> See Video
+/videos/:id/edit -> Edit Video
+/videos/:id/delete -> Delete Video
+/videos/upload -> Upload Video
---
+++ src/init.js
@@ -0,0 +1,16 @@
+import "regenerator-runtime";
+import "dotenv/config";
+import "./db";
+import "./models/Video";
+import "./models/User";
+import "./models/Comment"
+import app from "./server";
+
+
+const PORT = 4000;
+
+
+const handleListening = () =>
+    console.log(`✅ Server listening ton port http://localhost:${PORT} 🚀`)
+
+app.listen(PORT, handleListening);
